0f4c9566b52acd9f55349070296a3c192657b008
Naavneeth21gojoVR <vrnaavneeth@gmail.com>
2026-07-20 12:05:31 +0530

    Configure port and start script for deployment

--- server.js
+++ server.js
@@ -23,3 +23,9 @@ app.use("/api/users", require("./Routers/UserRouter"));
 
 connectDB();
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
